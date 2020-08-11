15a83a25668e89e00f51a5b9d75c101c186f0312
Tim Greene <tim.g.greene@gmail.com>
2020-08-11 16:35:00 +0100

    Create minimum viable web service
    Using muuntaja, reitit and jetty.

--- project.clj
+++ project.clj
@@ -1,9 +1,23 @@
 (defproject ila-prototype "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :description "ila-project prototype project"
+  :url "https://github.com/ELiTLtd/ila-prototype"
+  :min-lein-version "2.0.0"
+  :dependencies [[clj-http "3.10.1"]
+                 [metosin/muuntaja "0.6.7"]
+                 [metosin/potpuri "0.5.2"]
+                 [metosin/reitit "0.5.2"]
+                 [org.clojure/clojure "1.10.1"]
+                 [org.eclipse.jetty/jetty-server "9.4.28.v20200408"]
+                 [ring/ring-core "1.8.1"]
+                 [ring/ring-jetty-adapter "1.8.1"]
+                 [ring/ring-servlet "1.8.1"]]
+  :plugins [[reifyhealth/lein-git-down "0.3.6"]]
+  :profiles {:dev {:dependencies [[ring/ring-devel "1.8.1"]]}
+             :testing {:dependencies [[ring/ring-mock "0.4.0"]]}
+             :uberjar {:aot :all}}
+  :middleware [lein-git-down.plugin/inject-properties]
+  :repositories [["public-github" {:url "git://github.com"}]
+                 ["private-github" {:url "git://github.com"
+                                    :protocol :ssh}]]
   :main ^:skip-aot ila-prototype.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :target-path "target/%s")
--- src/ila_prototype/core.clj
+++ src/ila_prototype/core.clj
@@ -1,7 +1,51 @@
 (ns ila-prototype.core
-  (:gen-class))
+  (:require muuntaja.core
+            ring.middleware.params
+            reitit.coercion.spec
+            reitit.ring
+            reitit.ring.coercion
+            reitit.ring.middleware.muuntaja
+            [ring.adapter.jetty :as ring-jetty]))
+
+(def sample-resource
+  {:name :resources/sample
+   :get {:handler (fn [_request]
+                    {:status 200
+                     :body {:status "OK"}})}})
+
+(def router
+  (reitit.ring/router
+   [["/sample" sample-resource]]
+   {:data {:muuntaja muuntaja.core/instance
+           :middleware [reitit.ring.middleware.muuntaja/format-middleware
+                        reitit.ring.coercion/coerce-exceptions-middleware
+                        reitit.ring.coercion/coerce-request-middleware
+                        reitit.ring.coercion/coerce-response-middleware]}}))
+
+(def root-handler
+  (reitit.ring/ring-handler router
+                            (reitit.ring/create-default-handler)))
+
+(defonce server-instance (atom nil))
+
+(defn stop-server
+  []
+  (when @server-instance
+    (.stop @server-instance)))
+
+(defn start-server
+  [port]
+  (stop-server)
+  (reset! server-instance
+          (ring-jetty/run-jetty #'root-handler
+                                {:port port
+                                 :join? true})))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [port (Integer/parseInt (get (System/getenv) "PORT" "3000"))]
+    (.join (start-server port))))
+
+(comment
+  ;; start the server from within an nrepl session
+  (start-server 3000))
