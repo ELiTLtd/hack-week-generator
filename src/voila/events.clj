7537562e2e8ef35fdd43fcdb7b73d75e99d79ea0
enanae <ane.espeseth@gmail.com>
2020-09-01 11:21:26 +0200

    updated gigya id to a more generalisable global id

--- src/voila/events.clj
+++ src/voila/events.clj
@@ -1,5 +1,6 @@
 (ns voila.events
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [clojure.set :as set]))
 
 (s/def :events/learning-event   ;; we might want to create a system-event too, but have no example files atm
   (s/keys :req [:events/actor
@@ -7,12 +8,12 @@
                 :events/event-id
                 :events/event-timestamp
                 :events/ua]
-          :opt [:events/assigned-path-id])) ;; might not need this.
+          :opt [:events/assigned-path-id])) ;; might not need this, gwen's not sure what it does.
 
 (s/def :events/actor
-  (s/keys :req [:events/actor-id
+  (s/keys :req [:events/actor-local-id                      ;; e.g. C1 ID
+                :events/actor-global-id                     ;; e.g. Gigya ID
                 :events/country
-                :events/gigya-id
                 :events/org
                 :events/role]))
 
@@ -30,14 +31,14 @@
 
 (s/def :events/activity-id string?)                         ;; "entity item-code" in xAPI spec
 (s/def :events/activity-timestamp int?)                     ;; "entity timestamp"
-(s/def :events/actor-id string?)                            ;; "actor uuid"
+(s/def :events/actor-global-id string?)                     ;; "ext-user-id"
+(s/def :events/actor-local-id string?)                      ;; "actor uuid"
 (s/def :events/assigned-path-id string?)                    ;; unknown
 (s/def :events/bundle-codes (s/coll-of string?))
 (s/def :events/class-id string?)
 (s/def :events/country string?)                             ;; can technically also get this from gigya
 (s/def :events/event-id string?)
 (s/def :events/event-timestamp int?)                        ;; "created" / "modified"
-(s/def :events/gigya-id string?)                            ;; "ext-user-id"
 (s/def :events/org string?)
 (s/def :events/product-code string?)
 (s/def :events/result string?) ;; score (scorable task), html text (writing task) or s3 bucket file (audio task)
@@ -46,17 +47,3 @@
                       "downloaded" }) ;; excluding teacher events such as "evaluated" for now since we're missing example files
 (s/def :events/ua string?)
 
-(s/explain :events/learning-event {:events/actor {:events/actor-id "ane123"
-                                                  :events/country "NO"
-                                                  :events/gigya-id "123ane"
-                                                  :events/org "elit-or-npd?"
-                                                  :events/role "student"}
-                                   :events/activity {:events/activity-id "activity1"
-                                                     :events/activity-timestamp 12345
-                                                     :events/class-id "task-class1"
-                                                     :events/product {:events/product-code "abc"
-                                                                      :events/bundle-codes #{"bundle1"}}
-                                                     :events/verb "submitted"}
-                                   :events/event-id "event1"
-                                   :events/event-timestamp 1234567
-                                   :events/ua "some-platform-data"})
