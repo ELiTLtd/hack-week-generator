b723bedf8a24cdc3758fd4035ae288f4c036104e
enanae <ane.espeseth@gmail.com>
2020-09-01 11:22:43 +0200

    User file from Clojure lesson

---
+++ src/voila/users.clj
@@ -0,0 +1,23 @@
+(ns voila.users)
+
+(def first-names ["Alice" "Bob" "Charlie" "Eggbert" "Yoongi" "Kim"])
+(def surnames ["Park" "Min" "Smith" "Yeoung"])
+(def hobbies ["singing" "dancing" "painting" "ping-pong"])
+
+(defn generate-user []
+  {:first-name (rand-nth first-names)
+   :surname (rand-nth surnames)
+   :age (rand-int 100)
+   :hobbies (set(repeatedly (inc (rand-int (count hobbies))) #(rand-nth hobbies)))})
+
+(defn generate-users
+  [num]
+  (zipmap (range num)
+          (take num
+                (repeatedly generate-user ))))
+
+(def users (generate-users 10))
+
+(defn get-user
+  [user-id]
+  (get users user-id))
