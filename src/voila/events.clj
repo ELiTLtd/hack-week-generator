4d541f1d38128603a80e272a3499a185d3509b17
enanae <ane.espeseth@gmail.com>
2020-09-01 16:09:29 +0200

    Added some explanations to spec

--- src/voila/events.clj
+++ src/voila/events.clj
@@ -1,43 +1,44 @@
 (ns voila.events
   (:require [clojure.spec.alpha :as s]
-            [clojure.set :as set]))
+            [clojure.set :as set]
+            [clojure.test.check.generators :as gen]))
 
 (s/def :events/learning-event   ;; we might want to create a system-event too, but have no example files atm
-  (s/keys :req [:events/actor
+  (s/keys :req-un [:events/actor
                 :events/activity
                 :events/event-id
                 :events/event-timestamp
                 :events/ua]
-          :opt [:events/assigned-path-id])) ;; might not need this, gwen's not sure what it does.
+          :opt-un [:events/assigned-path-id])) ;; might not need this, gwen's not sure what it does.
 
 (s/def :events/actor
-  (s/keys :req [:events/actor-local-id                      ;; e.g. C1 ID
+  (s/keys :req-un [:events/actor-local-id                      ;; e.g. C1 ID
                 :events/actor-global-id                     ;; e.g. Gigya ID
                 :events/country
                 :events/org
                 :events/role]))
 
-(s/def :events/activity
-  (s/keys :req [:events/activity-id
-                :events/activity-timestamp
-                :events/class-id
-                :events/product
-                :events/verb]
-          :opt [:events/result]))
+(s/def :events/activity                                     ;; "Entity" in xAPI spec
+  (s/keys :req-un [:events/activity-id
+                   :events/activity-timestamp
+                   :events/class-id
+                   :events/product
+                   :events/verb]
+          :opt-un [:events/result]))
 
 (s/def :events/product
-  (s/keys :req [:events/product-code
-                :events/bundle-codes]))
+  (s/keys :req-un [:events/product-code
+                   :events/bundle-codes]))
 
 (s/def :events/activity-id string?)                         ;; "entity item-code" in xAPI spec
 (s/def :events/activity-timestamp int?)                     ;; "entity timestamp"
-(s/def :events/actor-global-id string?)                     ;; "ext-user-id"
-(s/def :events/actor-local-id string?)                      ;; "actor uuid"
+(s/def :events/actor-global-id string?)                     ;; "ext_user_id" / "extStudentID"
+(s/def :events/actor-local-id uuid?)                        ;; "actor uuid" / "actorid"
 (s/def :events/assigned-path-id string?)                    ;; unknown
 (s/def :events/bundle-codes (s/coll-of string?))
-(s/def :events/class-id string?)
-(s/def :events/country string?)                             ;; can technically also get this from gigya
-(s/def :events/event-id string?)
+(s/def :events/class-id uuid?)                              ;; "classid" / "groupid"
+(s/def :events/country string?)                             ;; we can technically also get this from gigya
+(s/def :events/event-id string?)                            ;; "uuid"
 (s/def :events/event-timestamp int?)                        ;; "created" / "modified"
 (s/def :events/org string?)
 (s/def :events/product-code string?)
@@ -46,4 +47,5 @@
 (s/def :events/verb #{"attempted" "closed" "completed" "evaluated" "experienced" "launched" "submitted"
                       "downloaded" }) ;; excluding teacher events such as "evaluated" for now since we're missing example files
 (s/def :events/ua string?)
+
 
