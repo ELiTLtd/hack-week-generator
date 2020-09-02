c8704e39d0af1fe87a6e5a0ec489e11de5c9efb3
enanae <ane.espeseth@gmail.com>
2020-09-02 14:09:43 +0200

    Created simple generator for content

--- src/voila/content.clj
+++ src/voila/content.clj
@@ -1,6 +1,6 @@
 (ns voila.content
   (:require [clojure.spec.alpha :as s]
-            [clojure.spec.gen.alpha :as g]))
+            [clojure.spec.gen.alpha :as gen]))
 
 (s/def :content/content-instance   ;; we might want to create a system-event too, but have no example files atm
   (s/keys :req [:content/uuid
@@ -9,9 +9,14 @@
           :opt [:content/class
                 :content/meta-tags]))
 
-(s/def :content/uuid string?)
+(s/def :content/uuid uuid?)
 (s/def :content/name string?)
 (s/def :content/body string?)
 (s/def :content/class string?)
-(s/def :content/meta-tags (s/coll-of string?))
+(s/def :content/meta-tags (s/coll-of string? :distinct true :count (rand-int 5)))
 
+(defn generate-content
+  []
+  (nth (gen/sample (s/gen :content/content-instance 10)) 9))
+
+(generate-content)
