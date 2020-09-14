98085aa326b9058d7537efea5e9eb6a363048be5
GitHub <github-action@users.noreply.github.com>
2020-09-14 16:43:33 +0000

    Version 1.0.11-SNAPSHOT [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject voila "1.0.10"
+(defproject voila "1.0.11-SNAPSHOT"
   :description "ila-project prototype project"
   :url "https://github.com/ELiTLtd/voila"
   :min-lein-version "2.0.0"
